e99a8df54d5bd5d038026ab5c1d92c01824c6fbf
Christopher <cd.duke@gmail.com>
2017-10-26 12:44:33 -0500

    update

--- Strings.java
+++ Strings.java
@@ -4,23 +4,22 @@ public class Strings {
 	public static void main(String[] args) {
 		String bookTitle;
 		bookTitle = "The Lord of the Rings";
-		String wordChoice = "Ring";
+		String wordChoice = "Rings";
 		
 		if (bookTitle.contains(wordChoice)) {
 			System.out.println("The book contains the word " + wordChoice);
 		}
 		
 		String browser = "Chrome";
-		// (browser == "chrome")
 		if (browser.equalsIgnoreCase("chrome")) {
 			System.out.println("The browser is chrome");
 		}
 		
-		String firstName = "Chris";
+		String firstName = "Christopher";
 		String lastName = "Duke";
-		String SSN = "555-55-5555";
+		String SSN = "555555555";
 		
-		System.out.println("There are " + SSN.length() + " digits in your SSN.");
+		System.out.println("There are " + SSN.length() + " digits in your Social Security Number.");
 		
 		System.out.print(firstName.substring(0, 1));
 		System.out.print(lastName.substring(0, 3));
